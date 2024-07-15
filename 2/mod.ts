513ebffc5d3621c283d8b1b91bb08615038f12a2
Gnlow <gnlow@protonmail.com>
2024-07-16 01:35:14 +0900

    Update mod.ts

--- 2/mod.ts
+++ 2/mod.ts
@@ -9,43 +9,45 @@ id colon indent
 dedent
 `
 
-const input = `
-    id colon indent
-        id colon expr nl
-        id colon expr nl
-    dedent
-`.trim().replaceAll(/\s+/g, " ")//.split(" ")
+const parse =
+(rule: Record<string, string>) =>
+(input: string) => {
+    let output = input
+    let prev = output
+    const list = []
+    console.log(Object.entries(rule))
 
-const map = {
+    while (true) {
+        for (const [from, to] of Object.entries(rule)) {
+            prev = output
+            const regex = new RegExp(
+                from
+                    .split(" ")
+                    .map(x => x + "\\d*")
+                    .join(" "),
+            )
+            output = output.replace(regex, match => {
+                console.log(`[${from} -> ${to}]`)
+                console.log(output, "\n")
+                list.push(match)
+                return to + (list.length - 1)
+            })
+            if (prev != output) break
+        }
+        if (prev == output) break
+    }
+
+    console.log(output)
+}
+
+parse({
     "id colon expr": "expr",
     "expr nl expr": "expr",
     "expr nl": "expr",
     "indent expr dedent": "expr",
-}
-
-let output = input
-let prev = output
-const list = []
-console.log(Object.entries(map))
-
-while (true) {
-    for (const [from, to] of Object.entries(map)) {
-        prev = output
-        const regex = new RegExp(
-            from
-                .split(" ")
-                .map(x => x + "\\d*")
-                .join(" "),
-        )
-        output = output.replace(regex, match => {
-            console.log(`[${from} -> ${to}]`)
-            console.log(output, "\n")
-            list.push(match)
-            return to + (list.length - 1)
-        })
-        if (prev != output) break
-    }
-    if (prev == output) break
-}
-
-console.log(output)
+})(`
+    id colon indent
+        id colon expr nl
+        id colon expr nl
+    dedent
+`.trim().replaceAll(/\s+/g, " "))
